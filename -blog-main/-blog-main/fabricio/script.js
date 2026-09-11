9a65f5bc2e52629def2f887e5c48ebf856e49f66
fabriciomanenti-glitch <fabricio.manenti@escola.pr.gov.br>
2026-09-11 10:15:38 -0300

    Add files via upload
    11/09

--- -blog-main/-blog-main/fabricio/script.js
+++ -blog-main/-blog-main/fabricio/script.js
@@ -1,28 +1,28 @@
 const botaoTema = document.querySelector("#btn-tema");
 
 if (botaoTema) {
-    botaoTema.addEventListener("click", function () {
-        document.body.classList.toggle("tema-escuro");
-    });
+  botaoTema.addEventListener("click", function () {
+    document.body.classList.toggle("tema-escuro");
+  });
 }
 
 const botoes = document.querySelectorAll(".btn-interacao");
 
 botoes.forEach(function (botao) {
-    let curtiu = false;
+  let curtiu = false;
 
-    botao.addEventListener("click", function botaoClicado() {
-        let texto = botao.querySelector("span");
-        let contador = parseInt(texto.textContent);
+  botao.addEventListener("click", function botaoClicado() {
+    let texto = botao.querySelector("span");
+    let contador = parseInt(texto.textContent);
 
-        if (curtiu === false) {
-            contador++;
-            curtiu = true;
-        } else {
-            contador--;
-            curtiu = false;
-        }
+    if (curtiu === false) {
+      contador++;
+      curtiu = true;
+    } else {
+      contador--;
+      curtiu = false;
+    }
 
-        texto.textContent = contador;
-    });
+    texto.textContent = contador;
+  });
 });
